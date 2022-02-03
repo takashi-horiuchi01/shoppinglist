00e07e94bc47ca3d1def7f02afc1926519d32fcb
takashi.horiuchi <takashi.horiuchi01@gmail.com>
2022-02-03 13:14:18 +0000

    fix_rev4

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
-  validates :family_name, presence: true, length: { maximum: 5 }
+  validates :family_name, presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
                     uniqueness: { case_sensitive: false }
